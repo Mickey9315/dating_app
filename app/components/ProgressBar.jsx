3ae42853595c77d633505b9587675756cde64f02
maddev4 <maddev049@gmail.com>
2025-01-05 22:52:59 -0500

    onboard

--- app/components/ProgressBar.jsx
+++ app/components/ProgressBar.jsx
@@ -2,7 +2,7 @@
 
 const ProgressBar = ({ totalSteps, currentStep }) => {
   return (
-    <div className="flex gap-2 w-full max-w-md mx-auto my-4">
+    <div className="flex gap-2 w-[calc(100%+20px)] ml-[-10px] my-4">
       {[...Array(totalSteps)].map((_, index) => (
         <div
           key={index}
